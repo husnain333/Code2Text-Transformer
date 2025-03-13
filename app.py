5eafd776f6b46d991387b79d1f07976eb25b538d
mhusnain333 <mhusnain3133@gmail.com>
2025-03-13 21:08:57 +0500

    app.py updated

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from models.code2text1 import CodeToPseudo
 
 # Set page config with a bright/light theme look
 st.set_page_config(
-    page_title="text to C++ Code and C++ Code to text Generator",
+    page_title="Text to Code and Code to Text Generator Using Transformers",
     page_icon=":sparkles:",
     layout="wide",
 )
@@ -80,44 +80,44 @@ pseudo_to_code_model = load_pseudo_to_code_model()
 code_to_pseudo_model = load_code_to_pseudo_model()
 
 # Add session state to keep track of user inputs
-if 'text_input' not in st.session_state:
-    st.session_state.text_input = ""
+if 'pseudocode_input' not in st.session_state:
+    st.session_state.pseudocode_input = ""
 if 'cpp_code_input' not in st.session_state:
     st.session_state.cpp_code_input = ""
 
 # Streamlit application title
-st.title("text to C++ Code and C++ Code to text Generator")
+st.title("Text to Code and Code to Text Generator using Transformers")
 
 # Sidebar for user input
-st.sidebar.header("User Input")
+st.sidebar.header("Coversion Type")
 
 # Option to choose the conversion type
 conversion_type = st.sidebar.selectbox("Select Conversion Type", 
-                                       ("text to C++ Code", "C++ Code to text"))
+                                       ("Pseudocode to C++ Code", "C++ Code to Pseudocode"))
 
-# Input text area for text or C++ code
-if conversion_type == "text to C++ Code":
-    text_input = st.text_area("Enter text:", value=st.session_state.text_input)
-    st.session_state.text_input = text_input
+# Input text area for pseudocode or C++ code
+if conversion_type == "Pseudocode to C++ Code":
+    pseudocode_input = st.text_area("Enter Pseudocode:", value=st.session_state.pseudocode_input)
+    st.session_state.pseudocode_input = pseudocode_input
     
     if st.button("Generate C++ Code"):
-        if text_input:
+        if pseudocode_input:
             with st.spinner("Generating C++ code..."):
-                cpp_code = pseudo_to_code_model.generate_code(text_input)
+                cpp_code = pseudo_to_code_model.generate_code(pseudocode_input)
             st.subheader("Generated C++ Code:")
             st.code(cpp_code, language="cpp")
         else:
-            st.error("Please enter text to generate C++ code.")
+            st.error("Please enter pseudocode to generate C++ code.")
 else:
     cpp_code_input = st.text_area("Enter C++ Code:", value=st.session_state.cpp_code_input)
     st.session_state.cpp_code_input = cpp_code_input
     
-    if st.button("Generate text"):
+    if st.button("Generate Pseudocode"):
         if cpp_code_input:
-            with st.spinner("Generating text..."):
-                text = code_to_pseudo_model.generate_text(cpp_code_input)
-            st.subheader("Generated text:")
-            st.code(text)
+            with st.spinner("Generating pseudocode..."):
+                pseudocode = code_to_pseudo_model.generate_pseudocode(cpp_code_input)
+            st.subheader("Generated Pseudocode:")
+            st.code(pseudocode)
         else:
-            st.error("Please enter C++ code to generate text.")
+            st.error("Please enter C++ code to generate pseudocode.")
 
